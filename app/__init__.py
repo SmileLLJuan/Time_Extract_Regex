25418a8db206e50e0b02dd25ddb7c7dc5d4608ea
lilijuan11 <lilijuan11@hikvision.com.cn>
2021-07-29 11:23:57 +0800

    add:时间抽取

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/29 10:51
+# @Author  : lilijuan
+# @File    : __init__.py.py
+import sys,os
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
